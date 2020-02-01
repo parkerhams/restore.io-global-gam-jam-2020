92a443cecb7e9d2a4876b69ce7345d7b12e0a439
Parker Hamilton <parkerhams@gmail.com>
2020-01-31 20:50:05 -0600

    vegan working on conveyor belt physics

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGear : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, -10f, Space.Self);
+    }
+}
---
+++ Assets/Scripts/RepairObjectSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairObjectSpawner : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject repairItemPrefab;
+
+    private float timer = 3.5f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <= 0f)
+        {
+            SpawnItem();
+            timer = 3.5f;
+        }
+    }
+
+    private void SpawnItem()
+    {
+        GameObject repairItemInstance = Instantiate(repairItemPrefab, 
+            this.transform.position, Quaternion.identity);
+        print("Object instantiated!");
+    }
+}
